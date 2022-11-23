c627dcda433dd0b2d17d80b06b6cb13f64808e5b
ryanhausen <rhausen@ucsc.edu>
2022-11-23 16:33:05 -0500

    added notebook for making labels for snippets

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 Python
 - kaggle (this requires some setup, if not setup already)
--
+- spacy
--- ml_pipeline/data/transform_documents_to_snippets.py
+++ ml_pipeline/data/transform_documents_to_snippets.py
@@ -1,27 +1,58 @@
-from typing import Iterable, List
+from typing import Iterable, List, Tuple
 from dataclasses import dataclass
+
+import spacy
+
 from ml_pipeline.data.repository import DataSample
+
+nlp = spacy.load("en_core_web_trf")
 
 @dataclass
 class TrainingSample:
-    contains_dataset: float
-    mask_labels: List[float]
-    maked_sample: str
-    unmasked_sample:str
+    contains_dataset: float # snipet contains dataset 0<=x<=1
+    snippet: str            # snippet of text
+    token_labels: List[int] # 0 for no dataset, 1 for dataset
+    document_text: str
+    document_id: str
 
 
 def split_document_into_snippets(
     document:DataSample,
-    max_snippet_length:int,
-    beyond_sentence:bool,
+    n_sentences_per_snippet:int,
 ) -> List[TrainingSample]:
     document, label = document
 
     text_only = " ".join(list(map(lambda x: x["text"], document)))
 
+    processed = nlp(text_only)
+
+    for i in range(0, len(processed.sents), n_sentences_per_snippet):
+        snippet = processed.sents[i:i+n_sentences_per_snippet].text
+
     snippets = []
 
 
+def string_is_acronym(string:str) -> bool:
+    return all(map(lambda x: x.isupper(), string))
+
+
+
+def label_in_snippet(snippet:str, processed_label:Tuple[str, str]) -> bool:
+    # This is pretty much the goal of the whole project, so it's a bit tricky
+
+    expansion, acronym = processed_label
+    complete_label = expansion + " (" + acronym + ")"
+
+    if complete_label in snippet:
+        return True
+
+    if expansion in snippet:
+        return True
+
+    if acronym in snippet:
+        return True
+
+    return False
 
 def document_generator(source) -> Iterable[DataSample]:
     document_ids = source.get_ids()
@@ -29,6 +60,7 @@ def document_generator(source) -> Iterable[DataSample]:
     for document_id in document_ids:
         data_sample = source.get_by_ids([document_id])[0]
         yield data_sample
+
 
 
 
